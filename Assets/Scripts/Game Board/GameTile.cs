24eb8b31d8cb509a82e34f0e953ee27d67209aec
gjasay <gabeasay@gmail.com>
2024-08-30 21:12:56 -0400

    created basic gameboard functionality

--- Assets/Scripts/Game Board/GameTile.cs
+++ Assets/Scripts/Game Board/GameTile.cs
@@ -1,18 +1,42 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class GameTile : MonoBehaviour
 {
+    public GamePiece CurrentlyHeldPiece { get; set; } //The game piece currently held by the game tile
+    private GameboardManager _gameboardManager; //Reference to the GameboardManager
+
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        
+        _gameboardManager = GameObject.Find("GameboardManager").GetComponent<GameboardManager>(); //Get a reference to the GameboardManager
+        transform.SetParent(GameObject.Find("GameboardManager").transform); //Set the GameTile's parent to the GameboardManager
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        DetectTouch();
+    }
+
+    /*---------------------------------------------------------
+     * Detect if the player has touched the game tile
+     ----------------------------------------------------------*/
+    private void DetectTouch()
+    {
+        if (Input.touchCount > 0)
+        {
+            //Get the touch position
+            Touch touch = Input.GetTouch(0);
+            Vector3 touchPosition = Camera.main.ScreenToWorldPoint(touch.position);
+            touchPosition.z = 0;
+
+            //Check if the touch position is over the game tile
+            if (GetComponent<Collider2D>() == Physics2D.OverlapPoint(touchPosition))
+            {
+                if (_gameboardManager.SelectedGameTile == this) return; //If this game tile is already selected, return
+
+                _gameboardManager.SelectedGameTile = this; //Set the selected game tile to this game tile
+            }
+        }
     }
 }
--- Assets/Scripts/Game Board/GameboardManager.cs
+++ Assets/Scripts/Game Board/GameboardManager.cs
@@ -1,18 +1,64 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class GameboardManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
+    public GameTile SelectedGameTile { get; set; } // The currently selected game tile
+    [SerializeField] private GameObject _gameTilePrefab; //Reference to the GameTile prefab
+    [SerializeField] private GameObject _orangeTadpolePrefab; //Reference to the orange tadpole prefab
+    [SerializeField] private GameObject _purpleTadpolePrefab; //Reference to the purple tadpole prefab
+    private GameObject _selectedGamePiecePrefab; //Reference to the selected game piece prefab
+    private GameTile[,] _gameTiles; //2D array of GameTile objects
 
+    private void Start()
+    {
+        //Create a 6x6 game board
+        CreateGameboard(6, 6);
+
+        //Testing
+        _selectedGamePiecePrefab = _orangeTadpolePrefab;
     }
 
-    // Update is called once per frame
-    void Update()
+
+    /*---------------------------------------------------------
+     * Create a game board with the specified width and height
+     * @param width The width of the game board
+     * @param height The height of the game board
+     * @param tileSize The size of each tile in the game board
+     ----------------------------------------------------------*/
+    public void CreateGameboard(int width, int height, float tileSize = 0.65f)
     {
-        
+        _gameTiles = new GameTile[width, height];
+
+        float boardWidth = width * tileSize;
+        float boardHeight = height * tileSize;
+
+        float startX = -boardWidth / 2 + tileSize / 2;
+        float startY = -boardHeight / 2 + tileSize / 2;
+
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                GameObject newTile = Instantiate(_gameTilePrefab, new Vector3(startX + x * tileSize, startY + y * tileSize, 0), Quaternion.identity);
+                GameTile gameTile = newTile.GetComponent<GameTile>();
+
+                _gameTiles[x, y] = gameTile;
+            }
+        }
     }
+
+    /*--------------------------------------------------------------
+     * This method is called when the place button is pressed
+     * Place a game piece on the selected game tile
+     * Communicate to the selected game tile that it is holding a game piece
+     -----------------------------------------------------------------------*/
+    public void PlaceGamePiece()
+    {
+        if (SelectedGameTile == null || SelectedGameTile.CurrentlyHeldPiece != null) return;
+
+        GameObject gamePiece =  Instantiate(_selectedGamePiecePrefab, SelectedGameTile.transform.position, Quaternion.identity);
+
+        SelectedGameTile.CurrentlyHeldPiece = gamePiece.GetComponent<GamePiece>();
+    }
+
 }
